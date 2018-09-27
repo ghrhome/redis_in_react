597e3cb06879b88eca9cc4d753c825ca00aa75b1
whobird <whobird@Whobird.local>
2018-09-27 14:18:10 +0800

    update all

--- redis_x.py
+++ redis_x.py
@@ -7,7 +7,7 @@ host="r-bp17f80de892ecb4.redis.rds.aliyuncs.com"
 port="6379"
 
 esc="47.97.213.17"
-command="./redis-cli -h 47.97.213.17 -a r-bp17f80de892ecb4:Ghrhome2009"
+command="./redis-cli -h 47.99.122.6 -a r-bp13fab1fb3823e4:Ghrhome2009"
 
 #redis-cli -h r-bp17f80de892ecb4.redis.rds.aliyuncs.com -a Ghrhome2009
 
